859ffb64cd3783ea51cdfa45f912c65e65c55d61
shayan <shayan13771717@gmail.com>
2020-11-30 12:57:08 +0330

    eleventh night of python

---
+++ python-the-hard-way11.py
@@ -0,0 +1,20 @@
+#ex40
+class song(object):
+    def __init__(self , lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+
+happy_bday = song(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = song(["They rally around tha family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
+

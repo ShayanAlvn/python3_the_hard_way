d443580f27664cc9d95b54ec4186580c40763955
shayan <shayan13771717@gmail.com>
2020-12-02 13:11:40 +0330

    thirteenth night of python

--- python-the-hard-way12.py
+++ python-the-hard-way12.py
@@ -16,7 +16,22 @@ class Employee:
 
     def apply_reise(self):
         self.pay = int(self.pay * self.amount_of_raise)
+   
+    @classmethod 
+    def set_raise_amt(cls , amount):#in classmethod cls is like self in other defs
+        cls.amount_of_raise = amount #we can change all employees amount_of_raise by "Emplotee.set_raise_amt(x)" 'just another way to do that'
 
+    @classmethod
+    def from_string(cls , emp_str):
+        first , last , pay = emp_str.split('-')
+        return cls(first , last , pay)
+
+    @staticmethod # we will use staticmethod whenever we don't want to have acces to any of class vars
+    def is_workday(day):# in this method we don't have any self or cls as first input
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+  
 
 
 emp_1 = Employee('corey' , 'Schafer' , 10000)
@@ -30,3 +45,22 @@ emp_2.apply_reise()
 print(emp_1.pay)
 print(emp_2.pay)
 print(Employee.num_of_emps)
+
+#creating another emp by string var manual
+emp_string_1 = 'John-doe-70000'
+emp_string_2 = 'Steve-smith-30000'
+emp_string_3 = 'mr-hello-50000'
+
+first , last , pay = emp_string_1.split('-')
+emp_3 = Employee(first , last , pay)
+print(emp_3.__dict__)
+
+#now i am going to create another emp by using a @classmethod's def 
+emp_4 = Employee.from_string(emp_string_2)
+print(emp_4.__dict__)
+
+# i am going to use @staticmethod 
+import datetime
+my_date = datetime.date(2020 , 4 , 10)
+
+print(Employee.is_workday(my_date))
